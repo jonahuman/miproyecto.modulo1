c89518eff148a33fd12c3fd36b300594ad0423e1
flerxay <bettercallfler@gmail.com>
2024-05-03 22:33:09 -0400

    intentando descencriptar por fuerza bruta

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class Main {
 
+    public Main() {
+
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -30,8 +34,6 @@ public class Main {
     }
 
 }
-
-
 
 // @author: Jonathan David.
 
