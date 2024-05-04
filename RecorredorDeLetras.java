cb9d29a3cf6130a412da9c321bebec817d4a7c2d
flerxay <bettercallfler@gmail.com>
2024-05-03 22:21:47 -0400

    3 de mayo ultimo dia

--- RecorredorDeLetras.java
+++ RecorredorDeLetras.java
@@ -1,23 +1,30 @@
 
 public class RecorredorDeLetras {
 
-    // se puede recorrer el alfabeto de cada carácter y en el alfabeto según clave que des en consola xd.
+    // este método recorre el carácter en el alfabeto según la clave dada xd.
 
     public char recorrerCaracter(char caracter, int clave) {
+
+        // verifica si el carácter es una letra, de los primeros xd.
 
         if (Character.isLetter(caracter)) {
 
             char base = Character.isUpperCase(caracter) ? 'A' : 'a';
 
+            // ya el nuevo carácter desplazado y lo devuelve además de calcularlo xd.
+
             return (char) ((caracter - base + clave) % 26 + base);
 
         }
+
+        // es el carácter no es una letra, te lo devuelve sin cambios de una xd.
 
         return caracter;
 
     }
 
 }
+
 
 // @author: Jonathan David.
 
