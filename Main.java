70c462351c6567d33ec98c05e1dedc504f9002c8
flerxay <bettercallfler@gmail.com>
2024-05-03 23:28:58 -0400

    menú finalizado

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // este main gestiona las acciones del usuario xd.
+
         Scanner scanner = new Scanner(System.in);
 
         Encriptador encriptador = new Encriptador();
@@ -15,7 +17,8 @@ public class Main {
 
         int opcion;
 
-        while (true) {
+        while (true) { // recomendado que por cada funcionaldiad
+                       // hacer un commit por cada modificación
 
             System.out.println("*********MENÚ***********");
 
@@ -34,6 +37,8 @@ public class Main {
             scanner.nextLine(); // haré un menú como esos que hago en duoc uc xd.
 
             switch (opcion) {
+
+            // switch o cambiar en español, en python creo que esto equivale al while xd.
 
                 case 1:
 
@@ -73,7 +78,7 @@ public class Main {
 
                     case 3:
 
-                        System.out.println("Introduce texto a descencriptar por fuerza bruta: ");
+                        System.out.println("-.-.-.-.-.-.-Introduce texto a descencriptar por fuerza bruta-.-.-.-.-.-.-: ");
 
                         texto = scanner.nextLine();
 
@@ -85,7 +90,7 @@ public class Main {
 
                         case 4:
 
-                            System.out.println("Saliendo del programa...");
+                            System.out.println("**********Saliendo del programa...***********");
 
                             scanner.close();
 
@@ -93,7 +98,9 @@ public class Main {
 
                             default:
 
-                                System.out.println("Opción no válida. Intente de nuevo.");
+                                System.out.println("********Opción no válida. Intente de nuevo.*********");
+
+                                // cada vez que quise imprimir use el sout xd.
 
                                 break;
 
